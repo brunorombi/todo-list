89f67eaa5846c4cfb321ddc9fb73d36037d9207d
Bruno Rombi <brunorombi@yahoo.com>
2026-04-26 01:02:14 -0300

    feat: delete todo and restructure of the functions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import "./style.css";
-import { createToDo, createProject, pushTodo, printTodos }from './todo.js'
+import { createTodo, createProject, pushTodo, printTodos }from './todo.js'
 
-const firstToDo = createToDo("Gym", "Leg-Day", "04/30/2026", "High");
-const secondToDo = createToDo("Study", "Pair-Programming", "04/27/2026", "Low");
-const defaultProject = createProject("default");
-pushTodo(firstToDo, defaultProject);
-pushTodo(secondToDo, defaultProject);
-printTodos(defaultProject);
+const firstToDo = createTodo("Gym", "Leg-Day", "04/30/2026", "High");
+const secondToDo = createTodo("Study", "Pair-Programming", "04/27/2026", "Low");
+
+printTodos();
--- src/todo.js
+++ src/todo.js
@@ -1,27 +1,51 @@
-export const createToDo = function(title, description, dueDate, priority) {
-        return {
+const todos = [];
+const project = createProject('Default');
+todos.push(project);
+
+export function createTodo(title, description, dueDate, priority) {
+        const todo = {
                 id: crypto.randomUUID(),
                 title,
                 description,
                 dueDate,
                 priority
         }
-        
-}
+        project.todos.push(todo);
+        return todo;
+}     
 
-export const createProject = function(title) {
-        return {
+export function createProject(title) {
+        const project = {
                 id: crypto.randomUUID(),
                 title,
                 todos: []
-        }        
+        }
+        todos.push(project);
+        return project;    
 }
 
-export const pushTodo = function(todo, project) {
-        project.todos.push(todo);
+export function pushTodo(todo, id){
+        todos.forEach(project => {
+                if(project.id.includes(id)) {
+                        project.todos.push(todo);
+                }
+        })
 }
 
-export const printTodos = function(project) {
-        console.log(project.todos);
+export function printTodos() {
+        console.log(todos);
 }
+
+export const deleteTodo = function(todo, project) {
+        todos.forEach(el => {
+                el.todos.forEach((element, index) => {
+                        if(element === todo)
+                        el.todos.splice(index, 1);
+                });
+        });
+}
+
+
+
+
  
